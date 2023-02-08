99dead6444a490edd8d02b89a15236a390703546
Priscila <priscilasartori2@gmail.com>
2023-02-08 10:18:02 -0300

    feat: Requisito 2 - Crie o endpoint GET /talker/:id

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const app = express();
 app.use(express.json());
 
 const HTTP_OK_STATUS = 200;
+const HTTP_NOTFOUND_STATUS = 404;
 const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar
@@ -17,6 +18,14 @@ app.get('/talker', async (req, res) => {
   if (!speakers) {
     res.status(HTTP_OK_STATUS).send([]);
   } res.status(HTTP_OK_STATUS).send(speakers);
+});
+
+app.get('/talker/:id', async (req, res) => {
+  const speakers = await talkerReadFile();
+  const speaker = speakers.find(({id}) => id === Number(req.params.id))
+  if (!speaker) {
+    res.status(HTTP_NOTFOUND_STATUS).send({"message": "Pessoa palestrante não encontrada"});
+  } res.status(HTTP_OK_STATUS).send(speaker);
 });
 
 app.listen(PORT, () => {
